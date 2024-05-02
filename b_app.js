65136dee3c51a9f0783cdd5f7ed8c59b23ed9379
Ansh Mani Tripathi <anshmanitripathi171203@gmail.com>
2024-05-02 19:44:09 +0530

    Javascript

---
+++ b_app.js
@@ -0,0 +1,11 @@
+let light = "green";
+
+if(light == "red"){
+    console.log("stop");
+} else if(light == "green"){
+    console.log("go");
+} else if(light == "yellow"){
+    console.log("wait");
+} else{
+    console.log("wrong color");
+}
