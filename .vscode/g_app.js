51932e3c467fa36dae5cde3ffb022bc598dc2526
Ansh Mani Tripathi <anshmanitripathi171203@gmail.com>
2024-05-03 16:37:47 +0530

    Javascript

---
+++ .vscode/g_app.js
@@ -0,0 +1,26 @@
+// object literals in js
+
+
+let house = {
+    sofa : 2,
+    chair : 4,
+    name : "D179",
+    fridge : 1
+}
+
+house.block = "D";
+house.chair = 5;
+
+
+let dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+
+let person = {
+    name : "Ansh",
+    age : 19,
+    city : "GKP "
+};
+
+person.city = "New Delhi";
+person.country = "India";
