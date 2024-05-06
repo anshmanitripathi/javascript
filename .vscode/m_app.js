0ca0ea5f1effeac21f7ce624884c0b27c7018c41
Ansh Mani Tripathi <anshmanitripathi171203@gmail.com>
2024-05-07 01:00:43 +0530

    Javascript

---
+++ .vscode/m_app.js
@@ -0,0 +1,27 @@
+let inp = document.querySelector("input");
+
+// inp.addEventListener("keyup", function(event){
+//     console.log(event);
+//     console.log("Key is pressed.")
+// })
+
+inp.addEventListener("keyup", function(event){
+        console.log("code = ", event.code);
+        if(event.code == "ArrowDown"){
+            console.log("character moves downward.")
+        }else if(event.code == "ArrowUp"){
+            console.log("character moves upward.")
+        }else if(event.code == "ArrowRight"){
+            console.log("character moves Right.")
+        }else if(event.code == "ArrowLeft"){
+            console.log("character moves Left.")
+        }
+    })
+
+
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    alert("form submitted");
+})
